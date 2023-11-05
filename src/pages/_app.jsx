e793b0022cafba67b299b1d3cfb24595eff1c0eb
Ernestolop <ernestodaniellopez504@gmail.com>
2023-11-05 15:52:18 -0300

    Add imports
    Added fonts and baseline in the _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,20 @@
-import '@/styles/globals.css'
+import CssBaseline from '@mui/material/CssBaseline';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+//<meta name="viewport" content="initial-scale=1, width=device-width" /> TODO: add in the layout
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+
+  )
 }
 
 export default App;
